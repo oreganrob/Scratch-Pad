6f3e3f4dec034df026550daa6570c7361794831c
fatlog <robertoregan@eircom.net>
2013-12-10 21:22:47 +0000

    initial commit

---
+++ src/com/example/evies/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.evies;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+//import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //super.setIntergerProperty("splashscreen", R.drawable.splash);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
